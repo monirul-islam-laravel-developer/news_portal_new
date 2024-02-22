6fd9abb7794e95f51fce68293cd4a1e80017ec13
Md Arman Ali <mdarmanali070@gmail.com>
2024-02-22 09:26:21 +0600

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
-use Intervention\Image\Image;
+use Intervention\Image\Facades\Image;
 
 class Post extends Model
 {
